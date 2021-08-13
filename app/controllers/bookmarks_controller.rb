d20535e6dcd6c2dbdd2b81866a8b8e367380f52d
eric thanadabouth <ericthanadabouth@yahoo.fr>
2021-08-13 16:01:51 +0200

    not finish yet

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,10 +5,11 @@ class BookmarksController < ApplicationController
   end
 
   def create
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list_id = params[:list_id]
     if @bookmark.save
-      redirect_to list_path(@bookmark.list_id)
+      redirect_to list_path(@list)
     else
       render 'new'
     end
